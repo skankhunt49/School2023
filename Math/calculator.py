47fee01cc0663e198a0654f6c8b23e8ee1d9c91f
Isaac Turner <skankhunt49@outlook.com.au>
2023-02-09 10:05:23 +0800

    working on calculator for prem

--- Math/calculator.py
+++ Math/calculator.py
@@ -4,17 +4,50 @@ import math
 from gekko import GEKKO
 import pandas as pd
 
-print ("Choose Mode graphing, normal or algebraic")
-mode = input("Mode :")
+cont = "y"
+contWhole = "y"
 
-if mode == "graphing" :
-  pass
-elif mode == "normal" :
-  print("Input First Value :")
-  value1 = input()
-  operator = input("Input Operator :")
-  print
-elif mode == "algebraic" :
-  pass
-else :
-  print("ERROR")
+while contWhole == "y" :
+  contWhole = input("Continue? y or n :")
+  print ("Choose Mode graphing, normal or algebraic")
+  mode = input("Mode :")
+
+#when it iterates the whole loop it misses the if statement
+  if mode == "graphing" :
+    pass
+
+  elif mode == "normal" :
+    while cont == "y" :
+      print("Input First Value :")
+      value1 = float(input())
+      operator = input("Input Operator :")
+      print("Input Second Value :")
+      value2 = float(input())
+
+      if operator == "+" :
+        result = value1 + value2
+        print(result)
+        cont = input("Continue? y or n :")
+      elif operator == "-" :
+        result = value1 - value2
+        print(result)
+        cont = input("Continue? y or n :")
+      elif operator == "*" :
+        result = value2 * value1
+        print(result)
+        cont = input("Continue? y or n :")
+      elif operator == "/" :
+        result = value1 / value2
+        print(result)
+        cont = input("Continue? y or n :")
+      elif operator == "^" :
+        result = pow(value1, value2)
+        print(result)
+        cont = input("Continue? y or n :")
+      else :
+        print("ERROR")
+
+  elif mode == "algebraic" :
+    pass
+  else :
+    print("ERROR")
